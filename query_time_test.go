1524eb478418a2d5f2b164f2c1f124b95874e11e
Daniel Cannon <daniel@danielcannon.co.uk>
2013-09-08 14:21:51 +0100

    Fix to time tests to ensure that the time format is set to default

--- query_time_test.go
+++ query_time_test.go
@@ -7,7 +7,7 @@ import (
 
 func (s *RethinkSuite) TestTime(c *test.C) {
 	var response time.Time
-	err := Time(1986, 11, 3, 12, 30, 15, "Z").Run(session).One(&response)
+	err := Time(1986, 11, 3, 12, 30, 15, "Z").TimeFormat("").Run(session).One(&response)
 	c.Assert(err, test.IsNil)
 	c.Assert(response.Equal(time.Date(1986, 11, 3, 12, 30, 15, 0, time.Local)), test.Equals, true)
 }
@@ -38,7 +38,7 @@ func (s *RethinkSuite) TestEpochTime(c *test.C) {
 func (s *RethinkSuite) TestISO8601(c *test.C) {
 	var t1, t2 time.Time
 	t2, _ = time.Parse("2006-01-02T15:04:05-07:00", "1986-11-03T08:30:00-07:00")
-	err := ISO8601("1986-11-03T08:30:00-07:00").Run(session).One(&t1)
+	err := ISO8601("1986-11-03T08:30:00-07:00").TimeFormat("").Run(session).One(&t1)
 	c.Assert(err, test.IsNil)
 	c.Assert(t1.Equal(t2), test.Equals, true)
 }
@@ -47,7 +47,7 @@ func (s *RethinkSuite) TestInTimezone(c *test.C) {
 	loc, err := time.LoadLocation("MST")
 	c.Assert(err, test.IsNil)
 	var response []time.Time
-	err = Expr(List{Now(), Now().InTimezone("-07:00")}).Run(session).One(&response)
+	err = Expr(List{Now(), Now().InTimezone("-07:00")}).TimeFormat("").Run(session).One(&response)
 	c.Assert(err, test.IsNil)
 	c.Assert(response[1].Equal(response[0].In(loc)), test.Equals, true)
 }
@@ -63,7 +63,7 @@ func (s *RethinkSuite) TestBetween(c *test.C) {
 	})
 	err := times.Filter(func(row Exp) Exp {
 		return row.During(Time(1986, 9, 3, 12, 30, 15, "Z"), Time(1986, 11, 3, 12, 30, 15, "Z"))
-	}).Count().Run(session).One(&response)
+	}).Count().TimeFormat("").Run(session).One(&response)
 
 	c.Assert(err, test.IsNil)
 	c.Assert(int(response.(float64)), test.Equals, 2)
@@ -72,7 +72,7 @@ func (s *RethinkSuite) TestBetween(c *test.C) {
 func (s *RethinkSuite) TestYear(c *test.C) {
 	var response interface{}
 
-	err := Time(1986, 12, 3, 12, 30, 15, "Z").Year().Run(session).One(&response)
+	err := Time(1986, 12, 3, 12, 30, 15, "Z").Year().TimeFormat("").Run(session).One(&response)
 
 	c.Assert(err, test.IsNil)
 	c.Assert(int(response.(float64)), test.Equals, 1986)
@@ -81,7 +81,7 @@ func (s *RethinkSuite) TestYear(c *test.C) {
 func (s *RethinkSuite) TestMonth(c *test.C) {
 	var response interface{}
 
-	err := Time(1986, 12, 3, 12, 30, 15, "Z").Month().Eq(December()).Run(session).One(&response)
+	err := Time(1986, 12, 3, 12, 30, 15, "Z").Month().Eq(December()).TimeFormat("").Run(session).One(&response)
 
 	c.Assert(err, test.IsNil)
 	c.Assert(response.(bool), test.Equals, true)
@@ -90,7 +90,7 @@ func (s *RethinkSuite) TestMonth(c *test.C) {
 func (s *RethinkSuite) TestDay(c *test.C) {
 	var response interface{}
 
-	err := Time(1986, 12, 3, 12, 30, 15, "Z").Day().Eq(Wednesday()).Run(session).One(&response)
+	err := Time(1986, 12, 3, 12, 30, 15, "Z").Day().Eq(Wednesday()).TimeFormat("").Run(session).One(&response)
 
 	c.Assert(err, test.IsNil)
 	c.Assert(response.(bool), test.Equals, true)
